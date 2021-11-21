bef54a4ba5f8e084523ccc5f9a2a4ddfc245668b
wendy12321 <88644797+wendy12321@users.noreply.github.com>
2021-11-21 21:50:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // const http = require('http');
 // const PORT = process.env.PORT || 3000; 
 
-app.set( 'port', ( process.env.PORT || 5000 ));
+app.set( 'port', ( process.env.PORT || 3000 ));
 
 
 
