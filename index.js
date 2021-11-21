9d5c7b2d2e85f7613e4f55839ae2f386f7045665
wendy12321 <88644797+wendy12321@users.noreply.github.com>
2021-11-21 20:41:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Categories = require("./models/category");
 const app = express();
 
 const http = require('http');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000; 
 
 // use layouts
 app.use(layouts);
